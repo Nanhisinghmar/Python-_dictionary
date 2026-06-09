eece9199d14f1068e5f7eb1dd05048a656562d96
Nanhisinghmar <kajalsinghmar80@gmail.com>
2026-06-09 20:41:12 +0530

    Add files via upload

---
+++ dictionary-keys.py
@@ -0,0 +1,15 @@
+# all key 
+student = {
+    "name": "Kajal",
+    "age": 25,
+    "city": "Hisar"}
+print(student.keys())
+
+#use loop
+student = {
+    "math": 45,
+    "science": 80,
+    "english": 60,
+    "history": 30 }
+for key in student:
+    print(key)
